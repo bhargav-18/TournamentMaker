612744f1897aefb23bfc3f6eb811a4f61f0adb9b
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2021-11-04 20:53:59 +0530

    bug fixes

--- app/src/main/java/com/example/tournamentmaker/mainactivity/mainfragments/ui/signout/SignOutDialogFragment.kt
+++ app/src/main/java/com/example/tournamentmaker/mainactivity/mainfragments/ui/signout/SignOutDialogFragment.kt
@@ -21,24 +21,26 @@ class SignOutDialogFragment : DialogFragment() {
             .setTitle("Sign out?")
             .setMessage("Do you wan to sign out from app?")
             .setPositiveButton("Yes") { _, _ ->
-                CoroutineScope(Dispatchers.IO).launch {
+                CoroutineScope(Dispatchers.Main).launch {
                     val option =
                         GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
                             .build()
                     val signInClient = GoogleSignIn.getClient(requireActivity(), option)
-                    if (signInClient.signOut().isSuccessful) {
-                        withContext(Dispatchers.Main) {
-                            Intent(requireContext(), AuthActivity::class.java).also {
-                                startActivity(it)
+                    signInClient.signOut().addOnCompleteListener {
+                        if (it.isSuccessful) {
+
+                            Intent(requireContext(), AuthActivity::class.java).also { intent ->
+                                startActivity(intent)
                                 requireActivity().finish()
+
                             }
-                        }
-                    } else {
-                        Firebase.auth.signOut()
-                        withContext(Dispatchers.Main) {
-                            Intent(requireContext(), AuthActivity::class.java).also {
-                                startActivity(it)
+                        } else {
+                            Firebase.auth.signOut()
+
+                            Intent(requireContext(), AuthActivity::class.java).also { intent ->
+                                startActivity(intent)
                                 requireActivity().finish()
+
                             }
                         }
                     }
